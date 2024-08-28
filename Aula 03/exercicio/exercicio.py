d6a5e1928793effa5af0cdf5a38a3a339d21e1c0
igor <igor89994@gmail.com>
2024-08-28 20:21:51 -0300

    exercico

---
+++ Aula 03/exercicio/exercicio.py
@@ -0,0 +1,21 @@
+frutas = [
+    "banana",
+    "abacaxi",
+    "abacate",
+    "uva",
+    "laranja",
+    "manga",
+    "amora",
+    "açaí",
+    "acerola",
+    
+
+
+]
+frutas.sort()
+frutas.append("pedrinho")
+frutas.reverse()
+y = 1
+for x in frutas:
+    print(y,"-",x)
+    y = y + 1
