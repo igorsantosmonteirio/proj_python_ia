309858c2062b1e7af4a5819a8f5082a6733cb330
igor <igor89994@gmail.com>
2024-08-28 18:56:44 -0300

    laço de repetição

---
+++ Aula 03/para02.py
@@ -0,0 +1,19 @@
+alunos = [
+    "Gustavo",
+    "joão",
+    "matheus",
+    "Paulo",
+    "Nilton",
+    "Ana",
+    "Bianca",
+    "isadora",
+    "victoria",
+    "kauan",
+]
+alunos.sort()
+alunos.append("Pedro")
+alunos.reverse()
+y = 1
+for x in alunos:
+     print(y,"-",x)
+     y = y + 1
